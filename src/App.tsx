d3330777294ce4437dc6e6b3c186f49db1f03638
raulruedaa <raulenrico1@gmail.com>
2024-12-31 18:38:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { SubjectsPage } from "./pages/SubjectsPage";
-import { SubjectPage } from "./pages/SubjectPage";
-import { ExercisePage } from "./pages/ExercisePage";
-import { AboutPage } from "./pages/AboutPage";
-import { AuthPage } from "./pages/AuthPage";
-import { PricingPage } from "./pages/PricingPage";
-import { ProfilePage } from "./pages/ProfilePage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { SubjectsPage } from './pages/SubjectsPage';
+import { SubjectPage } from './pages/SubjectPage';
+import { ExercisePage } from './pages/ExercisePage';
+import { AboutPage } from './pages/AboutPage';
+import { AuthPage } from './pages/AuthPage';
+import { PricingPage } from './pages/PricingPage';
 
 function App() {
-   return (
-      <BrowserRouter>
-         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/subjects" element={<SubjectsPage />} />
-            <Route path="/subjects/:subjectId" element={<SubjectPage />} />
-            <Route
-               path="/subjects/:subjectId/:topicId/:exerciseId"
-               element={<ExercisePage />}
-            />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-         </Routes>
-      </BrowserRouter>
-   );
+     return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/subjects" element={<SubjectsPage />} />
+        <Route path="/subjects/:subjectId" element={<SubjectPage />} />
+        <Route path="/subjects/:subjectId/:topicId/:exerciseId" element={<ExercisePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
