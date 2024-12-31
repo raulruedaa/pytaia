dd797bcec600b60dea9536b5b17e36c367c387a9
raulruedaa <raulenrico1@gmail.com>
2024-12-31 20:24:50 -0300

    dale

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -1,23 +1,25 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  icon?: React.ReactNode;
+   icon?: React.ReactNode;
+   type: string;
 }
 
-export function Input({ icon, ...props }: InputProps) {
-  return (
-    <div className="relative">
-      {icon && (
-        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
-          {icon}
-        </div>
-      )}
-      <input
-        {...props}
-        className={`w-full px-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-pink-500 focus:border-transparent transition-all ${
-          icon ? 'pl-12' : ''
-        }`}
-      />
-    </div>
-  );
+export function Input({ icon, type, ...props }: InputProps) {
+   return (
+      <div className="relative">
+         {icon && (
+            <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+               {icon}
+            </div>
+         )}
+         <input
+            type={type}
+            {...props}
+            className={`w-full px-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-pink-500 focus:border-transparent transition-all ${
+               icon ? "pl-12" : ""
+            }`}
+         />
+      </div>
+   );
 }
