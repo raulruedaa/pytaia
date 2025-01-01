18c2b4d2d2711628f3cdc3c56d9447bc3556ef50
Raul Rueda <raulenrico1@gmail.com>
2024-12-31 23:26:45 -0300

    Implementado possibilidade de rodar o site localmente

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import aspectRatio from "@tailwindcss/aspect-ratio";
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {},
+   },
+   plugins: [aspectRatio],
 };
